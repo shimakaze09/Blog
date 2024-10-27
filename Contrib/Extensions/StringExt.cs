be8b942cd897ec23e37737e56fda6295c52fee5f
John <yangtianzhuo970425@gmail.com>
2024-10-27 15:35:08 +1300

    Add StringExt class with Limit method

---
+++ Contrib/Extensions/StringExt.cs
@@ -0,0 +1,14 @@
+namespace Contrib.Extensions;
+
+public static class StringExt
+{
+    public static string Limit(this string str, int length)
+    {
+        if (str.Length <= length)
+        {
+            return str;
+        }
+
+        return str[..length];
+    }
+}
