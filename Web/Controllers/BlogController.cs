ff33d4dec5a1cc29ed47600d56d4f34087ca478f
John <yangtianzhuo970425@gmail.com>
2024-10-27 15:33:26 +1300

    Update .gitignore to include database files and remove unused entries

--- Web/Controllers/BlogController.cs
+++ Web/Controllers/BlogController.cs
@@ -1,12 +1,25 @@
+using FreeSql;
 using Microsoft.AspNetCore.Mvc;
+using Data.Models;
+using Web.ViewModels;
 
 namespace Web.Controllers;
 
 public class BlogController : Controller
 {
-    // GET
+    private readonly IBaseRepository<Post> _postRepo;
+    private readonly IBaseRepository<Category> _categoryRepo;
+    public BlogController(IBaseRepository<Post> postRepo, IBaseRepository<Category> categoryRepo)
+    {
+        _postRepo = postRepo;
+        _categoryRepo = categoryRepo;
+    }
+
     public IActionResult List()
     {
-        return View();
+        return View(new BlogList
+        {
+            Posts = _postRepo.Select.ToList()
+        });
     }
 }
---
+++ Web/ViewModels/BlogList.cs
@@ -0,0 +1,8 @@
+using Data.Models;
+
+namespace Web.ViewModels;
+
+public class BlogList
+{
+    public List<Post> Posts { get; set; }
+}
