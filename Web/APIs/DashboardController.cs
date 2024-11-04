42067a9c0e175a275a744c73f317947379c1a85a
John <yangtianzhuo970425@gmail.com>
2024-11-04 16:14:13 +1300

    Added dashboard interface

---
+++ Web/APIs/DashboardController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels.Response;
+
+namespace Web.Apis;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+[ApiExplorerSettings(GroupName = "common")]
+public class DashboardController : ControllerBase
+{
+    [HttpGet("[action]")]
+    public ApiResponse ClrStats()
+    {
+        return ApiResponse.Ok();
+    }
+}
