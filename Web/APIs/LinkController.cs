935b8bd9150cd7cf7313d5e11ae3b667bfc75587
John <yangtianzhuo970425@gmail.com>
2024-11-07 15:54:06 +1300

    Add exchange link interface

---
+++ Web/APIs/LinkController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Data.Models;
+using Web.Services;
+using Web.ViewModels.Response;
+
+namespace Web.Apis;
+
+[Authorize]
+[ApiController]
+[Route("Api/[controller]")]
+[ApiExplorerSettings(GroupName = "blog")]
+public class LinkController : ControllerBase
+{
+    private readonly LinkService _service;
+
+    public LinkController(LinkService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public List<Link> GetAll()
+    {
+        return _service.GetAll();
+    }
+
+    [HttpGet("{id:int}")]
+    public ApiResponse<Link> Get(int id)
+    {
+        var item = _service.GetById(id);
+        return item == null ? ApiResponse.NotFound() : new ApiResponse<Link>(item);
+    }
+}
