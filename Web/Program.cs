564cea9ad5d5ae23258f961941c678fc523217f8
John <yangtianzhuo970425@gmail.com>
2024-11-12 18:46:58 +1300

    Add a public service, currently only supports downloading files, using IHttpClientFactory

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,11 +7,15 @@ using Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var mvcBuilder = builder.Services.AddControllersWithViews(
     options => { options.Filters.Add<ResponseWrapperFilter>(); }
 );
-if (builder.Environment.IsDevelopment()) mvcBuilder.AddRazorRuntimeCompilation();
+
+// Enable Razor page dynamic compilation in development mode
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
 
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddFreeSql(builder.Configuration);
@@ -22,6 +26,7 @@ builder.Services.AddCors(options =>
         policyBuilder.AllowCredentials();
         policyBuilder.AllowAnyHeader();
         policyBuilder.AllowAnyMethod();
+        // policyBuilder.AllowAnyOrigin();
         policyBuilder.WithOrigins("http://localhost:8080");
     });
 });
@@ -29,8 +34,10 @@ builder.Services.AddSwagger();
 builder.Services.AddSettings(builder.Configuration);
 builder.Services.AddAuth(builder.Configuration);
 builder.Services.AddHttpContextAccessor();
+// Register IHttpClientFactory, reference: https://docs.microsoft.com/zh-cn/dotnet/core/extensions/http-client
+builder.Services.AddHttpClient();
 
-// Custom services
+// Register custom services
 builder.Services.AddScoped<BlogService>();
 builder.Services.AddScoped<CategoryService>();
 builder.Services.AddScoped<ConfigService>();
@@ -39,8 +46,9 @@ builder.Services.AddScoped<LinkService>();
 builder.Services.AddScoped<PhotoService>();
 builder.Services.AddScoped<PostService>();
 builder.Services.AddScoped<VisitRecordService>();
-builder.Services.AddSingleton<ThemeService>();
+builder.Services.AddSingleton<CommonService>();
 builder.Services.AddSingleton<Messages>();
+builder.Services.AddSingleton<ThemeService>();
 builder.Services.AddSingleton<PicLibService>();
 
 var app = builder.Build();
@@ -84,6 +92,7 @@ app.UseReDoc(options =>
 });
 
 app.MapControllerRoute(
-    "default",
-    "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
---
+++ Web/Services/CommonService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using Contrib.Utils;
+
+namespace Web.Services;
+
+/// <summary>
+/// Some common services
+/// </summary>
+public class CommonService
+{
+    private readonly ILogger<CommonService> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CommonService(ILogger<CommonService> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    /// <summary>
+    /// Download a file
+    /// </summary>
+    /// <param name="url">URL of the file to download</param>
+    /// <param name="savePath">Save path (must be a complete path)</param>
+    /// <returns>The name of the downloaded file, or null if the download failed</returns>
+    public async Task<string?> DownloadFile(string url, string savePath)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        try
+        {
+            var resp = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+
+            var fileName = GuidUtils.GuidTo16String() + Path.GetExtension(url);
+            var filePath = Path.Combine(savePath, WebUtility.UrlEncode(fileName));
+            await using var fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            await resp.Content.CopyToAsync(fs);
+
+            return fileName;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Error downloading file, information: {Error}", ex);
+            return null;
+        }
+    }
+}
