efc590f07e7eb3e084db904f24601518881a80bb
John <yangtianzhuo970425@gmail.com>
2024-11-12 17:09:12 +1300

    Add Configuration Model

---
+++ Data/Models/ConfigItem.cs
@@ -0,0 +1,13 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace Data.Models;
+
+public class ConfigItem
+{
+    [Column(IsIdentity = true, IsPrimary = true)]
+    public int Id { get; set; }
+
+    public string Key { get; set; }
+    public string Value { get; set; }
+    public string? Description { get; set; }
+}
