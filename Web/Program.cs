55df81ee06b264d6946722fb4e7be474427194d3
John <yangtianzhuo970425@gmail.com>
2024-11-15 13:04:29 +1300

    Add reverse proxy forwarding middleware

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,6 @@
 using Contrib.SiteMessage;
 using Data.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 using SixLabors.ImageSharp.Web.DependencyInjection;
 using Web.Extensions;
 using Web.Filters;
@@ -62,6 +63,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseImageSharp();
 // app.UseHttpsRedirection();
