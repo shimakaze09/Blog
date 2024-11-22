0da823b9a24d1514f92d2baeb9462fdab9a1f1e9
John <yangtianzhuo970425@gmail.com>
2024-11-23 12:02:26 +1300

    A completely new article list UI

--- Share/Extensions/StringExt.cs
+++ Share/Extensions/StringExt.cs
@@ -6,9 +6,15 @@ public static class StringExt
 {
     public static string Limit(this string str, int length)
     {
-        if (str.Length <= length) return str;
+        return str.Length <= length ? str : str[..length];
+    }
 
-        return str[..length];
+    /// <summary>
+    ///     Limits the display length of a string and adds ellipsis at the end
+    /// </summary>
+    public static string LimitWithEllipsis(this string str, int length)
+    {
+        return str.Length <= length ? str : $"{str[..length]}...";
     }
 
     public static string ToSHA256(this string source)
